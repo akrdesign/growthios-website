bd3ec9aa81279293314d38f75e6d0991b612c03c
akrdesign <akrdesign04@gmail.com>
2024-10-30 17:41:57 +0530

    add not found page in single case study

--- pages/case-studies/[title].tsx
+++ pages/case-studies/[title].tsx
@@ -4,14 +4,22 @@ import { SingleCaseStudyPage } from "@/templates/index";
 
 import useSmoothScroll from "@/hooks/use-smooth-scroll";
 import { getCaseStudyById } from "@/mocks/case-studies";
+import NotFound from "../404";
 
 
 export default function SingleCaseStudy() {
   useSmoothScroll();
   const router = useRouter();
 
+  // Wait for router to be ready before accessing query params
+  if (!router.isReady) return null;
+
   const caseStudyTitle = router.query.title;
   const caseStudy = getCaseStudyById(caseStudyTitle);
+
+  if (!caseStudy) {
+    return <NotFound />;
+  }
 
   return (
     <>
